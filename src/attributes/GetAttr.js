918879262a8d167e5c69fe6ea5acfbb5ad4a9c80
intertext.no <orstavik77@gmail.com>
2022-06-16 10:19:36 +0200

    GetAttr

--- src/attributes/GetAttr.js
+++ src/attributes/GetAttr.js
@@ -23,29 +23,30 @@ export function targetedGETAttr(target = "_blank") {
 
 //The [name] convention.
 // get all non-empty [name] descendants, but only one level deep in the [name] hierarchy.
-function getNames(root) {
-  return root.querySelectorAll(':root > [name]:not([name=""]), :not([name]) [name]:not([name=""])');
+function getNames(scope) {
+  return scope.querySelectorAll(':scope > [name]:not([name=""]),:scope :not([name]) [name]:not([name=""])');
 }
 
 //The [.value] convention.
 // 1. HTML 1: direct attribute on element wins, first [value], then [src].
-// 2. HTML 2: children attribute list. if the element has [name] and then lots of children with [name] attributes,
-//    then it is a list and made as such.
+// 2. HTML 2: [name]children list.
+//             If the element has [name] (no [value] nor [src]), and then lots of children with [name] attributes,
+//             then the value is a `;` separated lists of the [name] value of its children.
 // 3. JS: if the .value property has been declared on the element, we use that.
-// 4. Fallback solution: outerHTML.
+// 4. Fallback solution: outerHTML.                          //todo should we have outerHTML? or just null?
 function getValue(el) {
   if (el.hasAttribute("value")) return el.getAttribute("value");
   if (el.hasAttribute("src")) return el.getAttribute("src");
-  const childrenName = el.querySelectorAll(':root > [name]:not([name=""])');
-  return childrenName.length ? [...childrenName].map(el => el.getAttribute("name")) :
+  const childrenName = el.querySelectorAll(':scope > [name]:not([name=""])');
+  return childrenName.length ? [...childrenName].map(el => el.getAttribute("name")).join(";") :
     "value" in el ? el.value :
       el.outerHTML;
 }
 
 export class GETAttrWithValues extends Attr {
   onEvent(e) {
-    const query = getNames(this.ownerElement)            //todo do we need to encode the [name] attribute too?
-      .map(el => `${el.getAttribute("name")}=${encodeURIComponent(getValue(el))}`).join("&");
-    open(this.value + "?" + query);
+    const url = new URL(this.value);
+    url.search = new URLSearchParams([...getNames(this.ownerElement)].map(c => [c.getAttribute("name"), getValue(c)]));
+    open(url.href);
   }
 }
