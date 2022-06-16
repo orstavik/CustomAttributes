55859a8dd000cb59cf4c673ee8ce9933bc833ef5
intertext.no <orstavik77@gmail.com>
2022-06-16 10:42:58 +0200

    simplifying

--- src/attributes/GetAttr.js
+++ src/attributes/GetAttr.js
@@ -1,33 +1,27 @@
 import {getNames, getValue} from "./name_conventions.js";
 
-export class GETAttr extends Attr {
-  onEvent(e) {
-    open(this.value, "_self");
+function GetAttr(target, values) {
+  if (values) {
+    return class GETAttr extends Attr {
+      onEvent(e) {
+        const url = new URL(this.value);
+        url.search = new URLSearchParams([...getNames(this.ownerElement)].map(c => [c.getAttribute("name"), getValue(c)]));
+        open(url.href, target);
+      }
+    }
+  }
+  return class GETAttr extends Attr {
+    onEvent(e) {
+      open(this.value, target);
+    }
   }
 }
 
-export class GET_blankAttr extends Attr {
-  onEvent(e) {
-    open(this.value, "_blank");
-  }
-}
-
-export class GET_topAttr extends Attr {
-  onEvent(e) {
-    open(this.value, "_top");
-  }
-}
-
-export class GET_parentAttr extends Attr {
-  onEvent(e) {
-    open(this.value, "_parent");
-  }
-}
-
-export class GETAttrWithValues extends Attr {
-  onEvent(e) {
-    const url = new URL(this.value);
-    url.search = new URLSearchParams([...getNames(this.ownerElement)].map(c => [c.getAttribute("name"), getValue(c)]));
-    open(url.href);
-  }
-}
+export const GET_self_Attr = GetAttr("_self");
+export const GET_self_values_Attr = GetAttr("_self", true);
+export const GET_blank_Attr = GetAttr("_blank");
+export const GET_blank_values_Attr = GetAttr("_blank", true);
+export const GET_parent_Attr = GetAttr("_parent");
+export const GET_parent_values_Attr = GetAttr("_parent", true);
+export const GET_top_Attr = GetAttr("_top");
+export const GET_top_values_Attr = GetAttr("_top", true);
