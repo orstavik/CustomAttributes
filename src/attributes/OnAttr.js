e3af21c3cf5cadb05ed1ccefda2851460df3d039
intertext.no <orstavik77@gmail.com>
2022-06-17 15:12:42 +0200

    simplifying

--- src/attributes/OnAttr.js
+++ src/attributes/OnAttr.js
@@ -1,9 +1,9 @@
-function runMethods(at, e) {
+function runMethods(at, e, target) {
   for (let method of at.value.split(" ")) {
     try {
-      if (!(method in at.ownerElement))
-        throw `'.${method}' is not a function on element <${at.ownerElement.tagName}>. Is it a typo?`;
-      at.ownerElement[method](e);
+      if (!(method in target))
+        throw `'.${method}' is not a function on element <${target.tagName}>. Is it a typo?`;
+      target[method](e);
     } catch (err) {
       const event = new Event("error", err);
       event.defaultAction = _ => console.error(err);
@@ -14,13 +14,39 @@ function runMethods(at, e) {
 
 export class OnAttr extends Attr {
   onEvent(e) {
-    runMethods(this, e);
+    runMethods(this, e, this.ownerElement);
   }
 }
 
 export class OnceAttr extends Attr {
   onEvent(e) {
-    runMethods(this, e);
+    runMethods(this, e, this.ownerElement);
+    this.remove();
+  }
+}
+
+export class OnHostAttr extends Attr {
+  onEvent(e) {
+    runMethods(this, e, this.ownerElement.getRootNode().host);
+  }
+}
+
+export class OnceHostAttr extends Attr {
+  onEvent(e) {
+    runMethods(this, e, this.ownerElement.getRootNode().host);
+    this.remove();
+  }
+}
+
+export class OnParentAttr extends Attr {
+  onEvent(e) {
+    runMethods(this, e, this.ownerElement.parentNode);
+  }
+}
+
+export class OnceParentAttr extends Attr {
+  onEvent(e) {
+    runMethods(this, e, this.ownerElement.parentNode);
     this.remove();
   }
 }
