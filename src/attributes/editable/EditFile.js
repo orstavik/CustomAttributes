f23d26aff1dc4621383dfbde5111bf2920439fd4
intertext.no <orstavik77@gmail.com>
2022-07-20 16:38:33 +0200

    first draft of EditImage and FormData attributes.

---
+++ src/attributes/editable/EditFile.js
@@ -0,0 +1,29 @@
+export class ImageEditAttr extends Attr {
+
+  static async readImage(file) {
+    if (!file) return "";
+    return new Promise(resolve => {
+      const reader = new FileReader();
+      reader.addEventListener("load", () => resolve(reader.result), false);
+      reader.readAsDataURL(file);
+    });
+  }
+
+  async onEvent(e) {
+    // if (e.defaultPrevented || e.defaultAction)
+    //   return;
+    // this.ownerElement.blur(); //todo do we need this one?
+    const input = document.createElement("input");
+    input.type = "file";
+    input.click();
+    const edit = new CustomEvent("edit", {
+      composed: true,
+      bubbles: true,
+      detail: await ImageEditAttr.readImage(input.files[0])
+    });
+    //todo how to test for cancellations?
+    this.ownerElement.dispatchEvent(edit);
+  }
+
+
+}
